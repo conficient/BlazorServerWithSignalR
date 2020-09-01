0040a9b39bf260f87c6b133ed3ad42a1fbaad8d6
Howard Richards <dev@conficient.com>
2020-09-01 17:33:56 +0100

    Add project files.

---
+++ BlazorServerWithSignalR/TalkService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorServerWithSignalR
+{
+    public class TalkService
+    {
+        public TalkService()
+        {
+            history = new List<string>();
+        }
+
+        public Action<string> OnChange { get; set; }
+
+
+        // inform all users of new message
+        public Task SendAsync(string message)
+        {
+            // add to history
+            history.Add(message);
+            // ensure only last 10 shown
+            if (history.Count > 10) history.RemoveAt(0);
+            OnChange.Invoke(message);
+            return Task.FromResult(0);
+        }
+
+        private readonly List<string> history;
+
+        public IReadOnlyList<string> GetHistory() => history;
+    }
+}
---
+++ BlazorServerWithSignalR/TimerService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorServerWithSignalR
+{
+    public class TimerService : BackgroundService
+    {
+        private readonly TalkService talkService;
+
+        public TimerService(TalkService talkService)
+        {
+            this.talkService = talkService;
+            Console.WriteLine("TimerService created");
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("TimerService started");
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                // wait five seconds
+                await Task.Delay(5000, stoppingToken);
+
+                if (!stoppingToken.IsCancellationRequested)
+                {
+                    await talkService.SendAsync($"The time is {DateTime.Now}");
+                }
+            }
+        }
+    }
+}
